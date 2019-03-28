6b246c020f59b57002c16d5a08896d1a35a3244c
Ben Maydan <maydanb@gmail.com>
2019-03-28 15:37:03 -0700

    Starter code for connect four game

--- classes.py
+++ classes.py
@@ -7,11 +7,16 @@ class Chip:
         self.color = color
 
 
+    def __repr__(self):
+        return self.color
+
+
 class Player:
 
     def __init__(self, color, name):
         self.color = color
         self.name = name
+
 
     def play_chip(self):
         return Chip(self.color)
@@ -22,19 +27,56 @@ class Display:
     def __init__(self):
         pass
 
+
     def draw(self):
         pass
 
 
 class ConnectFour:
-
+    """
+    Column class
+    """
 
     def __init__(self, player_1, player_2, rows, cols):
-        pass
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.rows = rows
+        self.columns = cols
+        self.number_of_turns = 0
+
+        self.board = {}
+        for i in range(self.columns):
+            label = string.ascii_uppercase[i]
+            self.board[label] = []
 
 
     def play_turn(self):
-        pass
+        if self.number_of_turns % 2 == 0:
+            player = self.player_1
+        else:
+            player = self.player_2
+
+        name = player.name
+        print(name[0].upper() + name[1:] + "'s turn.'")
+        try:
+            column = input('Please choose a column: ')
+        except KeyError:
+            print('NOT A VALID KEY!')
+
+        if len(self.board[column]) == self.rows:
+            print('Column', column, 'is full')
+            return False
+
+        else:
+            self.board[column].append(player.play_chip())
+            self.number_of_turns += 1
+            print(self.board)
+            return True
+
 
     def show_state(self):
+        print(self.board)
+
+
+    def check_win(self):
         pass
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 """
 Main game loop
 """
+from classes import Player, ConnectFour
+
+
+p1 = Player('red', 'schlomo')
+p2 = Player('black', 'schlomit')
+game = ConnectFour(p1, p2, 6, 7)
+somebody_did_not_win = False
+
+while somebody_did_not_win == False:
+
+      game.show_state()
+      game.play_turn()
+      game.check_win()
