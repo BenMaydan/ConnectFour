5f3854de816a569026f45f3c106c7125a2132084
Ben Maydan <maydanb@gmail.com>
2019-04-07 23:21:12 -0700

    Fixed a rare bug that occurred when 4 chips of same color were in a row with nothing in between and also added comments and added a tie checker

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 import string
 import sys
 import numpy as np
+import random
 
 
 class Chip:
@@ -30,6 +31,13 @@ class ConnectFour:
     """
 
     def __init__(self, player_1, player_2, rows, cols):
+        random_first_player = {1:True, 2:False}
+        player_2_goes_first = random_first_player[random.randrange(1, 3)]
+        medium_to_switch_players = None
+        if player_2_goes_first == True:
+            medium_to_switch_players = player_1
+            player_1 = player_2
+            player_2 = medium_to_switch_players
         self.player_1 = player_1
         self.player_2 = player_2
         self.rows = rows
@@ -38,6 +46,8 @@ class ConnectFour:
         self.listPlayers = [player_1, player_2]
         self.ColumFullError = 'Cannot put chip in a full column!'
 
+        #The line below all the way to line number 72 is error checking
+        #For the correct type and length of both the row and the column
         self.RowLengthError = RowLengthError()
         self.ColumnLengthError = ColumnLengthError()
         self.RowTypeError = RowTypeError()
@@ -46,7 +56,6 @@ class ConnectFour:
         self.ColumnLengthErrorMSG = 'Invalid Column Length. Try a Number Greater Than 0!'
         self.RowTypeErrorMSG = 'Invalid type for "row" parameter. Use an integer!'
         self.ColumnTypeErrorMSG = 'Invalid type for "column" parameter. Use an integer!'
-
         try:
             if type(self.rows) != int:
                 raise RowTypeError
@@ -83,15 +92,14 @@ class ConnectFour:
             index += 1
 
         self.board_list = [[]]
-        #Generates correct number of nones
+        #Generates all of the rows in one list
+        #I.E. a 2d/2 dimensional list
         for x in range(self.rows):
             self.board_list.append([])
         #Puts correct number on None's in rows
         for row in self.board_list:
             for y in range(self.columns):
                 self.board_list[y].append(None)
-        #for x in self.board_list:
-        #    print(x)
 
 
     def play_turn(self):
@@ -105,6 +113,8 @@ class ConnectFour:
         player_name = str(player.name[0].upper() + str(player.name[1:]))
         player_color = str(player.color[0].upper() + player.color[1:])
 
+        #Error checking for the correct input and to make sure
+        #That the chip that was chosen can go in a correct column/row
         try:
             print('\n\n')
             column = input(player_name + '/' + player_color + ', please choose a column: ')
@@ -151,7 +161,8 @@ class ConnectFour:
         for n in range(numDashes):
             dashes += '-'
 
-        #Actually prints dashes and empty header (for now)
+        #Actually prints dashes and header
+        #Header is the letters A - G
         print(dashes)
         print(header)
         print(dashes)
@@ -175,8 +186,9 @@ class ConnectFour:
         Checks if there are 4 chips in a row
         Vertically
         """
-        #import main
         chips = ''
+
+        #Determines which player turn it is
         if self.number_of_turns % 2 == 0:
             player = self.player_1
         else:
@@ -253,7 +265,7 @@ class ConnectFour:
         array = np.array(self.board_list)
 
         #This vertically mirrors the original array so the wanted diagonals are
-        # lower-right-to-uppper-left.
+        # lower-right-to-upper-left.
         diagonals = [array[::-1,:].diagonal(i) for i in range(-array.shape[0]+1,array.shape[1])]
 
         # Opposite of the line above
@@ -287,11 +299,13 @@ class ConnectFour:
             if len(row) >= 4:
                 if row[-1] == None:
                     chip_color = None
+                    counter = 0
                 else:
                     chip_color = row[-1].color
                 for chip in row:
                     #Moves on to next iteration if there is no chip
                     if chip == None:
+                        counter = 0
                         continue
                     #If the current chip color is equal to the previous chips color, counter goes up
                     if chip.color == chip_color:
@@ -311,6 +325,39 @@ class ConnectFour:
                             return name
 
 
+    def check_if_tie(self, spacingBetweenLetters):
+        """
+        Checks if the whole board was filled up and no one won
+        """
+        counter = 0
+        for column in self.board:
+            if len(column) == self.columns:
+                counter += 1
+            if len(column) != self.columns:
+                break
+            if counter == len(rows):
+                #The computer likes to be fair,
+                #So it chose a random number including 1 and 2
+                #And used that to determine which player is printed first
+                #In the tie msg string
+                random_player_in_string = {1:True, 2:False}
+                player_1_goes_first_in_str = random_first_player[random.randrange(1, 3)]
+                player_1_name = self.player_1.name[0].upper() + self.player_1.name[1:]
+                player_2_name = self.player_2.name[0].upper() + self.player_2.name[1:]
+                if player_1_goes_first_in_str == True:
+                    msg = "Neither " + str(player_1_name) + " nor " + str(player_2_name) + " won."
+                else:
+                    msg = "Neither " + str(player_2_name) + " nor " + str(player_1_name) + " won."
+
+                #Prints the tie message
+                print(msg)
+                print("The game ended in a tie...")
+                print("\nThe last state of the board:")
+                self.show_state(spacingBetweenLetters)
+                sys.exit()
+
+
+#Errors I can raise for incorrect/broken input from player
 class ColumnFullError(Exception):
     pass
 class RowLengthError(Exception):
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 import numpy as np
 
 
-p1 = Player('red', 'Dan')
+p1 = Player('red', 'Lia')
 p2 = Player('black', 'Ben')
 #6, 7 is default board
 game = ConnectFour(p1, p2, 6, 7)
@@ -22,6 +22,7 @@ while somebody_did_not_win == False:
         game.check_win_vertical(spacingBetweenLetters)
         game.check_win_horizontal(spacingBetweenLetters)
         game.check_win_diagonally(spacingBetweenLetters)
+        game.check_if_tie(spacingBetweenLetters)
     except KeyboardInterrupt:
         print('\nStopping running processes ->')
         sys.exit()
